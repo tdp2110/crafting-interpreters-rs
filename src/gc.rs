c5de702c979f6f8f3c355ae5c3e1740008850334
Tom Peters <thomas.d.peters@gmail.com>
2020-10-02 23:58:50 -0400

    moving heap values from vec to hashmap

--- src/gc.rs
+++ src/gc.rs
@@ -1,5 +1,7 @@
 use crate::value;
 
+use std::collections::HashMap;
+
 enum GCData {
     String(String),
     Closure(value::Closure),
@@ -37,14 +39,21 @@ impl GCVal {
 pub struct Heap {
     bytes_allocated: usize,
     next_gc: usize,
-    values: Vec<GCVal>,
+    id_counter: usize,
+    values: HashMap<usize, GCVal>,
 }
 
 impl Default for Heap {
     fn default() -> Heap {
+        let next_gc = std::env::var("LOX_GC_TRIGGER_SIZE")
+            .ok()
+            .map(|env_str| env_str.parse::<usize>().ok())
+            .flatten()
+            .unwrap_or(1024 * 1024);
         Heap {
             bytes_allocated: 0,
-            next_gc: 1024 * 1024,
+            next_gc,
+            id_counter: 0,
             values: Default::default(),
         }
     }
@@ -53,56 +62,65 @@ impl Default for Heap {
 impl Heap {
     pub fn manage_str(&mut self, s: String) -> usize {
         self.bytes_allocated += s.len();
-        self.values.push(GCVal::from(GCData::String(s)));
-        self.values.len() - 1
+        let id = self.generate_id();
+        self.values.insert(id, GCVal::from(GCData::String(s)));
+        id
     }
 
     pub fn manage_closure(&mut self, c: value::Closure) -> usize {
         self.bytes_allocated += c.function.chunk.code.len();
         self.bytes_allocated += c.function.chunk.constants.len();
-        self.values.push(GCVal::from(GCData::Closure(c)));
-        self.values.len() - 1
+        let id = self.generate_id();
+        self.values.insert(id, GCVal::from(GCData::Closure(c)));
+        id
+    }
+
+    fn generate_id(&mut self) -> usize {
+        loop {
+            if !self.values.contains_key(&self.id_counter) {
+                return self.id_counter;
+            }
+            self.id_counter += 1;
+        }
     }
 
     pub fn get_str(&self, id: usize) -> &String {
-        self.values[id].data.as_str().unwrap()
+        self.values.get(&id).unwrap().data.as_str().unwrap()
     }
 
     pub fn get_closure(&self, id: usize) -> &value::Closure {
-        self.values[id].data.as_closure().unwrap()
+        self.values.get(&id).unwrap().data.as_closure().unwrap()
     }
 
     pub fn unmark(&mut self) {
-        for val in &mut self.values {
+        for val in self.values.values_mut() {
             val.is_marked = false;
         }
     }
 
     pub fn mark(&mut self, id: usize) {
-        self.values[id].is_marked = true;
+        self.values.get_mut(&id).unwrap().is_marked = true;
     }
 
     pub fn is_marked(&self, id: usize) -> bool {
-        self.values[id].is_marked
+        self.values.get(&id).unwrap().is_marked
     }
 
     pub fn children(&self, id: usize) -> Vec<usize> {
-        let val = &self.values[id];
+        let val = &self.values.get(&id).unwrap();
 
         if val.is_marked {
             return Vec::new();
         }
 
-        match self.values[id].data {
-            GCData::String(_) => return Vec::new(),
-            GCData::Closure(_) => {
-                return Vec::new();
-            }
+        match self.values.get(&id).unwrap().data {
+            GCData::String(_) => Vec::new(),
+            GCData::Closure(_) => Vec::new(),
         }
     }
 
     pub fn sweep(&mut self) {
-        self.values.retain(|val| val.is_marked)
+        self.values.retain(|_, val| val.is_marked)
     }
 
     pub fn should_collect(&self) -> bool {
