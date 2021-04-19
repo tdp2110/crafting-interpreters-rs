99c16140b61fae833edd9d406142ad01015990b2
Tom Peters <thomas.d.peters@gmail.com>
2021-04-19 00:01:21 -0400

    use rustyline for line handling in repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,13 +1,19 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 use crate::expr;
 use crate::parser;
 use crate::scanner;
 use crate::treewalk_interpreter;
 
-use std::io::{self, BufRead, Write};
 use std::sync::atomic::Ordering;
+
+static HISTORY_FILE: &str = ".repl-history.txt";
 
 pub fn run() {
     let mut interpreter: treewalk_interpreter::Interpreter = Default::default();
+    let mut rl = Editor::<()>::new();
+    rl.load_history(HISTORY_FILE).ok();
     println!(
         "============================================\n\
          Welcome to lox! using tree-walk interpreter.\n\
@@ -23,38 +29,40 @@ pub fn run() {
     }
 
     loop {
-        print!(">>> ");
-        io::stdout().flush().unwrap();
-        let mut line = String::new();
-        let stdin = io::stdin();
-        let nchar = stdin
-            .lock()
-            .read_line(&mut line)
-            .expect("Could not read line");
-        if nchar == 0 {
-            break;
-        }
+        let readline = rl.readline(">> ");
 
-        match scanner::scan_tokens(line) {
-            Ok(tokens) => match parser::parse(tokens) {
-                Ok(stmts) => {
-                    let stmts2: Vec<expr::Stmt> = stmts
-                        .iter()
-                        .map(|stmt| match stmt {
-                            expr::Stmt::Expr(expr) => expr::Stmt::Print(expr.clone()),
-                            _ => stmt.clone(),
-                        })
-                        .collect();
-                    match interpreter.interpret(&stmts2) {
-                        Ok(()) => {}
-                        Err(err) => println!("Runtime error: {}", err),
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                match scanner::scan_tokens(line) {
+                    Ok(tokens) => match parser::parse(tokens) {
+                        Ok(stmts) => {
+                            let stmts2: Vec<expr::Stmt> = stmts
+                                .iter()
+                                .map(|stmt| match stmt {
+                                    expr::Stmt::Expr(expr) => expr::Stmt::Print(expr.clone()),
+                                    _ => stmt.clone(),
+                                })
+                                .collect();
+                            match interpreter.interpret(&stmts2) {
+                                Ok(()) => {}
+                                Err(err) => println!("Runtime error: {}", err),
+                            }
+                        }
+                        Err(err) => println!("\nParse error: {}", err),
+                    },
+                    Err(err) => {
+                        println!("\nScanner error: {}", err);
                     }
                 }
-                Err(err) => println!("\nParse error: {}", err),
-            },
+            }
+            Err(ReadlineError::Interrupted) => break,
+            Err(ReadlineError::Eof) => break,
             Err(err) => {
-                println!("\nScanner error: {}", err);
+                println!("REPL Error: {:?}", err);
+                break;
             }
         }
     }
+    rl.save_history(HISTORY_FILE).unwrap();
 }
