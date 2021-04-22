b99c8af69a4f0ee1f41f7d8979c1099d3e7e3386
Tom Peters <thomas.d.peters@gmail.com>
2021-04-22 00:49:10 -0400

    don't require semicolon in repl (fixes #17)

--- src/repl.rs
+++ src/repl.rs
@@ -20,6 +20,66 @@ fn mk_interpreter() -> treewalk_interpreter::Interpreter {
     interpreter
 }
 
+fn eval_tokens(
+    interpreter: &mut treewalk_interpreter::Interpreter,
+    mut tokens: Vec<scanner::Token>,
+    recursion_depth: i64,
+) {
+    let handle_err = |err| {
+        println!("\nParse error: {:?}", err);
+    };
+    match parser::parse(tokens.clone()) {
+        Ok(stmts) => {
+            let stmts2: Vec<expr::Stmt> = stmts
+                .iter()
+                .map(|stmt| match stmt {
+                    expr::Stmt::Expr(expr) => expr::Stmt::Print(expr.clone()),
+                    _ => stmt.clone(),
+                })
+                .collect();
+            match interpreter.interpret(&stmts2) {
+                Ok(()) => {}
+                Err(err) => println!(
+                    "Runtime error: {}\n\n{}",
+                    err,
+                    interpreter.format_backtrace()
+                ),
+            }
+        }
+        Err(
+            err
+            @
+            parser::Error::TokenMismatch {
+                expected: scanner::TokenType::Semicolon,
+                found:
+                    scanner::Token {
+                        ty: scanner::TokenType::Eof,
+                        ..
+                    },
+                ..
+            },
+        ) => {
+            let expected_eof = tokens.pop().unwrap();
+
+            tokens.push(scanner::Token {
+                ty: scanner::TokenType::Semicolon,
+                lexeme: Vec::new(),
+                literal: None,
+                line: 0,
+                col: -1,
+            });
+            tokens.push(expected_eof);
+
+            if recursion_depth > 0 {
+                handle_err(err)
+            } else {
+                eval_tokens(interpreter, tokens, recursion_depth + 1)
+            }
+        }
+        Err(err) => handle_err(err),
+    }
+}
+
 pub fn run() {
     let mut interpreter = mk_interpreter();
     let mut line_reader = line_reader::LineReader::new(".repl-history.txt", ">>> ");
@@ -34,26 +94,7 @@ pub fn run() {
 
         match readline {
             line_reader::LineReadStatus::Line(line) => match scanner::scan_tokens(line) {
-                Ok(tokens) => match parser::parse(tokens) {
-                    Ok(stmts) => {
-                        let stmts2: Vec<expr::Stmt> = stmts
-                            .iter()
-                            .map(|stmt| match stmt {
-                                expr::Stmt::Expr(expr) => expr::Stmt::Print(expr.clone()),
-                                _ => stmt.clone(),
-                            })
-                            .collect();
-                        match interpreter.interpret(&stmts2) {
-                            Ok(()) => {}
-                            Err(err) => println!(
-                                "Runtime error: {}\n\n{}",
-                                err,
-                                interpreter.format_backtrace()
-                            ),
-                        }
-                    }
-                    Err(err) => println!("\nParse error: {:?}", err),
-                },
+                Ok(tokens) => eval_tokens(&mut interpreter, tokens, 0),
                 Err(err) => {
                     println!("\nScanner error: {}", err);
                 }
